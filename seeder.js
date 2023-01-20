f14d9803976bf4609c820c30c58e44c7fc015ec2
Barry <barry.alpha88@outlook.fr>
2023-01-20 10:09:26 +0100

    seeder

---
+++ seeder.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+
+
+
+//Load env vars 
+dotenv.config({ path: './config/.env'});
+const MONGO_URI = process.env.MONGO_URI
+
+
+// Load models 
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to DB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+    
+// Read JSON FILEs
+const bootcamps =JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// Delete Data
+const DeleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+if(process.argv[2] === '-i'){
+    importData();  
+}else if(process.argv[2] === '-d'){
+    DeleteData();
+}
