79ddd906791b772be4a44686a4e65d665aebd8af
Barry <barry.alpha88@outlook.fr>
2023-01-25 12:22:01 +0100

    auth/Encrypting

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ connectDB();
 // ROUTE FILES
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // INITIATE EXPRESS APP VARIABLE
 const app = express();
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers using app middlewear
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
