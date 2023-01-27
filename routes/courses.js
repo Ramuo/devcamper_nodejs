b60066379fc927643244acc38b271021b9b487af
Barry <barry.alpha88@outlook.fr>
2023-01-27 11:32:23 +0100

    permission

--- routes/courses.js
+++ routes/courses.js
@@ -13,7 +13,7 @@ const advancedResults = require('../middleware/advanceResults');
 
 const router = express.Router({mergeParams: true});
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
@@ -21,13 +21,13 @@ router
         path: 'bootcamp',
         select: 'name description'
     }), getCourses)
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteteCourse);
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteteCourse);
 
 
 
--- seeder.js
+++ seeder.js
@@ -13,6 +13,7 @@ const MONGO_URI = process.env.MONGO_URI
 // Load models 
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // connect to DB
 mongoose.connect(MONGO_URI, {
@@ -29,11 +30,16 @@ const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
 
         console.log('Data Imported...'.green.inverse);
         process.exit();
@@ -47,6 +53,7 @@ const DeleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
